f4f4fc6e676724e5a47bd96ab26e1ae79dd7ae39
Boris Novoselov <boris.novoselov602@gmail.com>
2024-05-29 15:11:15 +0300

    add db methods

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,6 +2,8 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val postgresql_driver_version: String by project
+val exposed_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.24"
@@ -24,6 +26,10 @@ repositories {
 }
 
 dependencies {
+    implementation("org.postgresql:postgresql:$postgresql_driver_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
@@ -36,5 +42,6 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml:2.3.11")
     testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version") 
+
 }
--- backend/src/main/kotlin/com/example/Application.kt
+++ backend/src/main/kotlin/com/example/Application.kt
@@ -10,8 +10,13 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.slf4j.LoggerFactory
+import kotlin.reflect.jvm.internal.impl.metadata.ProtoBuf.Constructor
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -19,11 +24,79 @@ fun main(args: Array<String>) {
 
 fun Application.module() {
     configureSerialization()
+    DatabaseFactory.init()
     configureDatabases()
     configureMonitoring()
     configureHTTP()
     configureSecurity()
     configureRouting()
+}
+
+@Serializable
+data class User(
+    val id : Int,
+    val name : String,
+    val password: String,
+    val telegram  : String?,
+    val confirmed: Boolean,
+    val is_admin: Boolean,
+    val is_ishtar: Boolean
+) {
+    constructor(_name:String, _password:String) :
+            this(0, _name, _password, null, false, false, false)
+}
+
+object DatabaseFactory {
+    fun init() {
+        val database =
+            Database.connect(
+                url = "jdbc:postgresql://localhost:5432/bablosis_db",
+                driver = "org.postgresql.Driver",
+                user = "ckaf",
+                password = "",
+            )
+    }
+    suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+}
+
+object Users : Table() {
+    val id = long("id").autoIncrement("users_id_seq").uniqueIndex()
+    val name = text("name")
+    val password = text("password")
+    val telegram = text("telegram").nullable()
+    val confirmed = bool("confirmed").default(false)
+    val isAdmin = bool("is_admin").default(false)
+    val isIshtar = bool("is_ishtar").default(false)
+}
+
+suspend fun addUser(user: User) {
+    DatabaseFactory.dbQuery {
+        Users.insert {
+            it[name] = user.name
+            it[password] = user.password
+            it[telegram] = user.telegram
+            it[confirmed] = user.confirmed
+            it[isAdmin] = user.is_admin
+            it[isIshtar] = user.is_ishtar
+        }
+    }
+}
+
+suspend fun userExists(userName: String): Boolean {
+    return DatabaseFactory.dbQuery {
+        Users.select { Users.name eq userName }
+            .count() > 0
+    }
+}
+
+suspend fun isValidUser(userName: String, password: String): Boolean {
+    return DatabaseFactory.dbQuery {
+        val user = Users.select { Users.name eq userName }.singleOrNull()
+        user?.let {
+            it[Users.password] == password
+        } ?: false
+    }
 }
 
 @Serializable data class LoginData(val username: String, val password: String)
@@ -42,20 +115,32 @@ fun Application.configureRouting() {
             val username = loginData.username
             val password = loginData.password
 
-            // val username = call.request.queryParameters["username"]
-            // val password = call.request.queryParameters["password"]
-
-            logger.info("GET SOME RESPONSE: $username")
-            println("GET SOME RESPONSE: $username")
             // Пример простой проверки
-            if (username == "admin" && password == "admin123") {
+            if (isValidUser(username, password)) {
                 logger.info("Authorization successful for user: $username")
-                call.respondText("Authorization successful!")
+                call.respondText("Some token")
             } else {
                 logger.info("Authorization failed for user: $username")
                 call.respondText("Authorization failed!")
             }
         }
+
+        post("/registration") {
+
+            val loginData = call.receive<LoginData>()
+            val username = loginData.username
+            val password = loginData.password
+
+            if (userExists(username)) {
+                call.respondText("This user already exists!")
+            } else {
+                logger.info("New user registered: $username")
+                val user = User(username, password)
+                addUser(user)
+                call.respondText("Some token")
+            }
+        }
+
         get("/hello") { call.respondText("Hello world!") }
     }
 }
